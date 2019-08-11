5a7be069ac2aa8de9066a09ca65138449b2d82c2
diliadis <JimmyHliad@gmail.com>
2019-08-12 01:14:58 +0300

    removing code that is transfered to movie_site_crawlers/main.py

--- movie_site_crawlers/imdb_crawler.py
+++ movie_site_crawlers/imdb_crawler.py
@@ -11,26 +11,3 @@ def get_movie_json(movie_title):
     data = json.load(urllib.request.urlopen(url))
     print(str(data))
     return data
-
-
-def load_csv_to_dataframe(filename):
-    data = None
-    if os.path.isfile(filename):
-        data = pd.read_csv(filename)
-    else:
-        print("The file "+str(filename)+" doesn't exists...")
-    return data
-
-
-def get_movie_info():
-
-    movies_info_list = []
-    data = load_csv_to_dataframe('datasets/odeon_csvs/dataset.csv')
-    movie_titles_list = data['movie_title'].unique().tolist()
-
-    for movie_title in movie_titles_list:
-        # many movie titles have aditional info about the language and the screening tech that uses
-        filtered_movie_title = movie_title.replace('(3D)', '').replace('(GR)', '').replace('(ENG)', '')
-        print('getting info for '+filtered_movie_title)
-        movie_json_info = get_movie_json(filtered_movie_title)
-        movies_info_list.append(movie_json_info)
